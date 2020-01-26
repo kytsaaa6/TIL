ed1eb8a856a43bf52f3dcca986d9b783aa4414f5
kytsaaa6 <kytsaaa6@naver.com>
2020-01-26 19:12:51 +0900

    Node - 모듈화 및 사용법

---
+++ mpart.js
@@ -0,0 +1,9 @@
+var M = {
+  v:'v',
+  f:function(){
+    console.log(this.v);
+  }
+}
+
+// M 객체를 모듈화 
+module.exports = M;
---
+++ muse.js
@@ -0,0 +1,10 @@
+// var M = {
+//   v:'v',
+//   f:function(){
+//     console.log(this.v);
+//   }
+// }
+
+// mpart.js의 M 객체를 part 변수에 삽입
+var part = require('./mpart.js');
+part.f();
