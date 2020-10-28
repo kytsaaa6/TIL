ba41b2144fac0bf27356215dcbf581ada639a532
권용민 <45849778+kytsaaa6@users.noreply.github.com>
2020-10-28 18:26:11 +0900

    2-1. AOP 기본 개념

---
+++ spring5/실습/sp5-chap07/src/main/java/chap07/ImpeCalculator.java
@@ -0,0 +1,15 @@
+package chap07;
+
+// 핵심 기능
+public class ImpeCalculator implements Calculator {
+
+	@Override
+	public long factorial(long num) {
+		long result = 1;
+		for (long i = 1; i <= num; i++) {
+			result *= i;
+		}
+		return result;
+	}
+
+}
---
+++ spring5/실습/sp5-chap07/src/main/java/chap07/RecCalculator.java
@@ -0,0 +1,14 @@
+package chap07;
+
+// 핵심 기능
+public class RecCalculator implements Calculator {
+
+	@Override
+	public long factorial(long num) {
+        if (num == 0)
+            return 1;
+        else
+            return num * factorial(num - 1);
+	}
+
+}
